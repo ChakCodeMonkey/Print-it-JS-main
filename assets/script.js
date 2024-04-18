5a59b89daa8c54379510b5dc580015394ff456ea
ChakCodeMonkey <147180818+ChakCodeMonkey@users.noreply.github.com>
2024-04-19 00:19:34 +0200

    changement de l'appel des images

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,18 @@
 const slides = [
     {
-        "image": "./assets/images/slideshow/slide1.jpg",
+        "image": "slide1.jpg",
         "tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
     },
     {
-        "image": "./assets/images/slideshow/slide2.jpg",
+        "image": "slide2.jpg",
         "tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
     },
     {
-        "image": "./assets/images/slideshow/slide3.jpg",
+        "image": "slide3.jpg",
         "tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
     },
     {
-        "image": "./assets/images/slideshow/slide4.png",
+        "image": "slide4.png",
         "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
     }
 ]
@@ -55,7 +55,7 @@ function displaySlide(index) {
     console.log(index);
     console.log(slides.length)
     taglineContainer.innerHTML = slides[index].tagLine;
-    bannerImg.src = slides[index].image;
+    bannerImg.src = `/assets/images/slideshow/${slides[index].image}`;
 
     const dots = document.querySelectorAll('.dot');
 
